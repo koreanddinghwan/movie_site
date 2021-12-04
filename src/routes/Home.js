5e9de0b6cf7cc7c6d2b45b76ad1ec89c54f2f745
koreanddinghwan <myunghwan0421@gmail.com>
2021-12-05 01:04:35 +0900

    이동

---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              id={movie.id}
+              key={movie.id}
+              title_long={movie.title_long}
+              medium_cover_image={movie.medium_cover_image}
+              genres={movie.genres}
+              summary={movie.summary}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
